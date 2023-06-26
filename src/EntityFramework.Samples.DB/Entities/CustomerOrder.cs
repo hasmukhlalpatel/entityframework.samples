2b1e36ea743bc8ad6242f8bec999853a4bdc1760
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2023-06-26 19:02:18 +0100

    refactoring

---
+++ src/EntityFramework.Samples.DB/Entities/CustomerOrder.cs
@@ -0,0 +1,12 @@
+﻿namespace EntityFramework.Samples.DB.Entities;
+
+public class CustomerOrder
+{
+    public int Id { get; set; }
+
+    public int CustomerId { get; set; }
+    public int OrderId { get; set; }
+
+    public Customer Customer { get; set; }
+    public Order Order { get; set; }
+}
---
+++ src/EntityFramework.Samples.DB/Entities/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace EntityFramework.Samples.DB.Entities;
+
+public class Order
+{
+    public int Id { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+    public DateTime UpdatedOn { get; set; }
+    public List<OrderItem> OrderItems { get; set; }
+
+}

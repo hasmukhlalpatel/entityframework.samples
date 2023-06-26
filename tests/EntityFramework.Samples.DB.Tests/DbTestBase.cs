8615f153989b8585fe09761ff8ba5df04cbd5cd9
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2023-06-26 22:53:12 +0100

    Aded more test with eager loading with child and grand childern loading

--- tests/EntityFramework.Samples.DB.Tests/DbTestBase.cs
+++ tests/EntityFramework.Samples.DB.Tests/DbTestBase.cs
@@ -21,8 +21,10 @@ public abstract class DbTestBase: IDisposable
                 new Customer { Id = 2, Name = "Test2" },
                 new CustomerOrder{ Id = 1, CustomerId = 1, OrderId = 1},
                 new Order { Id = 1, CreatedOn = DateTime.Today },
-                new OrderItem { Id = 1, OrderId = 1, ItemId = 1, CreatedOn = DateTime.Today },
-                new Item { Id = 1, Name = "TestItem1", Price = 1.23m }
+                new OrderItem { Id = 1, OrderId = 1, ItemId = 1, OrderPrice = 3.00m, CreatedOn = DateTime.Today },
+                new OrderItem { Id = 2, OrderId = 1, ItemId = 2, OrderPrice = 5.50m, CreatedOn = DateTime.Today },
+                new Item { Id = 1, Name = "TestItem1", Price = 1.23m },
+                new Item { Id = 2, Name = "TestItem2", Price = 5.00m }
             );
             dbContext.SaveChanges();
         }
--- tests/EntityFramework.Samples.DB.Tests/EagerLoadingTests.cs
+++ tests/EntityFramework.Samples.DB.Tests/EagerLoadingTests.cs
@@ -30,7 +30,22 @@ public class EagerLoadingTests : DbTestBase
         var order = orders.First();
         Assert.NotNull(order.OrderItems);
         Assert.Null(order.OrderItems.First().Item);
+    }
 
+    [Fact]
+    public void EagerLoadingWithChildrenItemFilter()
+    {
+        using var dbContext = CreateDbContext();
+        var orders = dbContext
+            .Orders
+            .Include(x => x.OrderItems.Where(x=>x.OrderPrice >= 5))
+            .Where(x => x.Id == 1)
+            .ToList();
+
+        Assert.Single(orders);
+        var order = orders.First();
+        Assert.NotNull(order.OrderItems);
+        Assert.Null(order.OrderItems.First().Item);
     }
 
     [Fact]
@@ -40,7 +55,7 @@ public class EagerLoadingTests : DbTestBase
         var orders = dbContext
             .Orders
             .Include(x => x.OrderItems)
-            .ThenInclude(x=>x.Item)
+            .ThenInclude(x => x.Item)
             .Where(x => x.Id == 1)
             .ToList();
 
@@ -48,5 +63,23 @@ public class EagerLoadingTests : DbTestBase
         var order = orders.First();
         Assert.NotNull(order.OrderItems);
         Assert.NotNull(order.OrderItems.First().Item);
+    }
+
+    [Fact]
+    public void EagerLoadingWithGranChildrenFilter()
+    {
+        using var dbContext = CreateDbContext();
+        var customerOrders = dbContext
+            .CustomerOrders
+            .Include(x => x.Order)
+            .ThenInclude(x => x.OrderItems.Where(y => y.OrderPrice > 5))
+            .ThenInclude(x => x.Item)
+            .Where(x => x.Id == 1)
+            .ToList();
+
+        Assert.Single(customerOrders);
+        var customerOrder = customerOrders.First();
+        Assert.NotNull(customerOrder.Order);
+        Assert.NotNull(customerOrder.Order.OrderItems.First().Item);
     }
 }
